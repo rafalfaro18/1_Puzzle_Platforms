c0f697ea63d6f163009d341ed2efbdc612f9539c
Rafael Alfaro <rafalfaro_18@outlook.com>
2019-07-23 20:33:34 -0600

    Adds a target location vector with widget to visualize in editor

--- PuzzlePlatforms/Source/PuzzlePlatforms/MovingPlatform.h
+++ PuzzlePlatforms/Source/PuzzlePlatforms/MovingPlatform.h
@@ -21,6 +21,9 @@ public:
 	UPROPERTY(EditAnywhere)
 	float Speed = 20;
 
+	UPROPERTY(EditAnywhere, Meta = (MakeEditWidget = true))
+	FVector TargetLocation;
+
 protected:
 
 	virtual void BeginPlay() override;
