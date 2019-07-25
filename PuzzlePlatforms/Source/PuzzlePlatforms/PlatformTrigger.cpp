e5b132a5f4d11fbfb1a72412a129726c7670caeb
Rafael Alfaro <rafalfaro_18@outlook.com>
2019-07-24 22:17:23 -0600

    Register endOverlap event of platform trigger

--- PuzzlePlatforms/Source/PuzzlePlatforms/PlatformTrigger.cpp
+++ PuzzlePlatforms/Source/PuzzlePlatforms/PlatformTrigger.cpp
@@ -24,6 +24,7 @@ void APlatformTrigger::BeginPlay()
 	Super::BeginPlay();
 	
 	TriggerVolume->OnComponentBeginOverlap.AddDynamic(this, &APlatformTrigger::OnOverlapBegin);
+	TriggerVolume->OnComponentEndOverlap.AddDynamic(this, &APlatformTrigger::OnOverlapEnd);
 }
 
 // Called every frame
@@ -35,4 +36,8 @@ void APlatformTrigger::Tick(float DeltaTime)
 
 void APlatformTrigger::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) {
 	UE_LOG(LogTemp, Warning, TEXT("Activated"));
+}
+
+void APlatformTrigger::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) {
+	UE_LOG(LogTemp, Warning, TEXT("Deactivated"));
 }
--- PuzzlePlatforms/Source/PuzzlePlatforms/PlatformTrigger.h
+++ PuzzlePlatforms/Source/PuzzlePlatforms/PlatformTrigger.h
@@ -31,4 +31,6 @@ private:
 	UFUNCTION()
 	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 };
