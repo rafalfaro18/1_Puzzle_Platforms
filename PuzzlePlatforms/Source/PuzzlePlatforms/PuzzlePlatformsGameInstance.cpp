151422a556a853b4969896354f2ada6da2727473
Rafael Alfaro <rafalfaro_18@outlook.com>
2019-07-25 00:28:17 -0600

    Host command listens for clients

--- PuzzlePlatforms/Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
+++ PuzzlePlatforms/Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
@@ -22,7 +22,7 @@ void UPuzzlePlatformsGameInstance::Host() {
 	UWorld* World = GetWorld();
 	if (!ensure(World != nullptr)) { return; }
 
-	World->ServerTravel("/Game/ThirdPersonCPP/Maps/ThirdPersonExampleMap");
+	World->ServerTravel("/Game/ThirdPersonCPP/Maps/ThirdPersonExampleMap?listen");
 }
 
 void UPuzzlePlatformsGameInstance::Join(const FString & Address) {
